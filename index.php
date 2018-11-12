13683c7ceb816e3d0fee754040c58b52440ef387
Alpronet <Alpronet@DESKTOP-6M8CGHA.mshome.net>
2018-11-11 22:07:54 -0200

    projeto completo

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
-<?php
+<?php 
+session_start();
+require_once("vendor/autoload.php");
 
-phpinfo();
+use \Slim\Slim;
 
-//
-//include 'Conn/Conn.php';
-//
-//
-//$teste = new Conn();
+$app = new Slim();
 
+$app->config('debug', true);
 
+require_once("functions.php");
+require_once("site.php");
+require_once("admin.php");
+require_once("admin-users.php");
+require_once("admin-categories.php");
+require_once("admin-products.php");
+require_once("admin-orders.php");
 
+$app->run();
 
+ ?>
